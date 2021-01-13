0d38980f5ff24730ce92816f3722e3db9737b097
OOgaboogaboog <74546565+OOgaboogaboog@users.noreply.github.com>
2021-01-13 17:06:16 -0500

    made a few minor modifacations

--- drawing.py
+++ drawing.py
@@ -3,9 +3,10 @@ import turtle
 # s = turtle.getscreen()
 
 
-def startDraw():
-  t = turtle.Turtle()
-
+def startDraw(word):
+  t = turtle.Turtle()#must be within function for game.py to work.
+  
+  turtle.getscreen()._root.attributes('-topmost', 1)
   '''
   Function to draw the gallows and start the game
   '''
@@ -38,8 +39,12 @@ def startDraw():
   t.right(90)
   t.forward(50)
   t.hideturtle() # hides the arrow that is displayed on screen
+  t.penup()
+  t.goto(0,-200)
+  t.write(word,move = False, align = 'center',font = ("Verdana",24,"normal"))
 
   print("HANGMAN".center(40, "~"))
   turtle.Screen().exitonclick()
 
-
+if __name__ == "__main__":
+  startDraw()
